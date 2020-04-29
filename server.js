48535da1e13f2009134162dd7ff8f602cc5a6529
Juan Camilo Isaza <camilo.isaza@zemoga.com>
2020-04-29 11:55:49 -0500

    setting headers to server

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ const routes = require('./server/routes')
 const app = express()
 
 app.use('/', routes.default)
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+})
 
 app.get('/', function (req, res) {
   res.send('<h1>Hello World!</h1>')
